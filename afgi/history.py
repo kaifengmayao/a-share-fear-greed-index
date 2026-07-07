0825d135c56535111b11ec6580f93dfa0a2339d9
kaifengmayao <kaifengmayao@gmail.com>
2026-07-07 22:22:36 +0800

    Retry full market breadth across Eastmoney hosts

--- afgi/history.py
+++ afgi/history.py
@@ -9,7 +9,7 @@ from typing import Any, Callable
 from .providers import DataProviders, INDEX_ALLOCATION_UNIVERSE, collect_attempts
 
 
-HISTORY_SCHEMA_VERSION = 7
+HISTORY_SCHEMA_VERSION = 8
 
 
 def save_market_history(
