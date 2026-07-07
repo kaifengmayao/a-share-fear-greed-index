a60cf0966e22d095dfa1800f32c476e8b11530ce
kaifengmayao <kaifengmayao@gmail.com>
2026-07-07 22:33:34 +0800

    Stabilize market breadth fetch and reuse cached result

--- afgi/history.py
+++ afgi/history.py
@@ -9,7 +9,7 @@ from typing import Any, Callable
 from .providers import DataProviders, INDEX_ALLOCATION_UNIVERSE, collect_attempts
 
 
-HISTORY_SCHEMA_VERSION = 9
+HISTORY_SCHEMA_VERSION = 10
 
 
 def save_market_history(
